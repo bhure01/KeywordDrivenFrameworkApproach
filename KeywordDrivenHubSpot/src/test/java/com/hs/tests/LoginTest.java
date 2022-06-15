b8b058e86b8bea0dd73b45a03c2f87025bcebcf5
bhure01 <dattatribhure@gmail.com>
2022-06-15 16:15:26 +0530

    Added: test script for LoginTest

---
+++ KeywordDrivenHubSpot/src/test/java/com/hs/tests/LoginTest.java
@@ -0,0 +1,16 @@
+package com.hs.tests;
+
+import com.hs.keyword.engine.KeyWordEngine;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    public KeyWordEngine keyWordEngine;
+
+    @Test
+    public void loginTest(){
+        keyWordEngine = new KeyWordEngine();
+        keyWordEngine.startExecution("login");
+    }
+
+}
